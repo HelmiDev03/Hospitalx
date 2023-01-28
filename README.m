4f2cb9127e2bfe7e1fd7b55da8ea7572b65559fe
Helmi Lakhder <102436033+HelmiDev03@users.noreply.github.com>
2023-01-29 00:03:40 +0100

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,2 @@
+a voiceboot using js to help patient deal alone with their problems while waiting for doctors <br>
+live : https://helmidev03.github.io/Hospitalx/
--- README.md
+++
@@ -1,2 +0,0 @@
-a voiceboot using js to help patient deal alone with their problems while waiting for doctors
-live : 
